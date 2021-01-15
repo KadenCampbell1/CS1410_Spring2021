873597ca8d3ca5da6b398e1d0a7adc8c0c1e9704
Kaden(LenLap) <10836246@my.uvu.edu>
2021-01-15 00:03:11 -0700

    Added to class notes; added tutorial project

---
+++ TutorialProject/MadLib.py
@@ -0,0 +1,7 @@
+color = input("enter color: ")
+plural_noun = input("plural noun: ")
+celeb = input("celeb: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celeb)
