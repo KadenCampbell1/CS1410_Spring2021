e02bc54b722f000e6c49618011cfdb1bb2654195
Kaden(LenLap) <10836246@my.uvu.edu>
2021-01-26 00:07:13 -0700

    Finished adding ratings to a dictionary

--- Python project/_Scripts/bookrecs.py
+++ Python project/_Scripts/bookrecs.py
@@ -6,18 +6,20 @@ for lines in book_file:
     line = (lines.strip().split(","))
     book_list.append(line)
 
+
+ratings_list = [x.strip().split() for x in ratings_file.readlines()]
+ratings_lyst = []
 ratings_dict = {}
-ratings_list = [x.strip() for x in ratings_file.readlines()]
-for ratings_value in ratings_list:
-    i = 0
-    if i == 0:
-        ratings_key = ratings_value
-    else:
-        ratings_dict[ratings_key] = ratings_value
-    i += 1
 
+for ratings_value in range(0, len(ratings_list)):
+    if ratings_value % 2 == 0:
+        name = ratings_list[ratings_value]
+    if ratings_value % 2 == 1:
+        rating = ratings_list[ratings_value]
+        ratings_lyst.append((name, rating))
 
-
+for key, lock in ratings_lyst:
+    ratings_dict[str(key).strip("\'[").strip("\']").lower()] = lock
 
 book_file.close()
 ratings_file.close()
@@ -25,7 +27,9 @@ ratings_file.close()
 def main():
     """add stuff"""
     print(book_list)
-    print(ratings_dict.items())
+    # print(ratings_list)
+    print(ratings_dict)
+    # print(ratings_lyst)
 
 if __name__ == "__main__":
     main()
