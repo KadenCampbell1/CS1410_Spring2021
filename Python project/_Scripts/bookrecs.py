8f3cb7ca32f875d27884f3b708a20400c1ef0280
Kaden(LenLap) <10836246@my.uvu.edu>
2021-01-27 00:34:00 -0700

    added to class notes; worked on book rec project

--- Python project/_Scripts/bookrecs.py
+++ Python project/_Scripts/bookrecs.py
@@ -24,12 +24,38 @@ for key, lock in ratings_lyst:
 book_file.close()
 ratings_file.close()
 
+def dot_product(x, y):
+    first_name_values = ratings_dict[x]
+    second_name_values = ratings_dict[y]
+    dot_list = [int(first_name_values[i]) * int(second_name_values[i]) for i in range(len(second_name_values))]
+    return sum(dot_list)
+
+def friends(target_name):
+    affinity_score = {}
+    friend_dict = {}
+    # number_tuple = ()
+    # friend_tuple = ()
+    for keys in ratings_dict.keys():
+        if keys == target_name:
+            affinity_score[-(dot_product(target_name, keys))] = None
+        else:
+            affinity_score[(dot_product(target_name, keys))] = keys
+    friend_dict[affinity_score.get(sorted(affinity_score.keys())[-2])] = sorted(affinity_score.keys())[-2]
+    friend_dict[affinity_score.get(sorted(affinity_score.keys())[-1])] = sorted(affinity_score.keys())[-1]
+    #return sorted(friend_dict.items()) #this returns key and value sorted by key
+    return sorted(friend_dict.keys())
+
+
+
+
 def main():
     """add stuff"""
     print(book_list)
     # print(ratings_list)
     print(ratings_dict)
     # print(ratings_lyst)
+    print(dot_product("ben", "moose"))
+    print(friends("ben"))
 
 if __name__ == "__main__":
     main()
