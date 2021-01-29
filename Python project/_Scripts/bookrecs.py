0a673ad30e1c992be63b4a39fbeaa95788666dc9
Kaden(LenLap) <10836246@my.uvu.edu>
2021-01-28 20:33:32 -0700

    Finished book recommend project

--- Python project/_Scripts/bookrecs.py
+++ Python project/_Scripts/bookrecs.py
@@ -11,10 +11,11 @@ ratings_list = [x.strip().split() for x in ratings_file.readlines()]
 ratings_lyst = []
 ratings_dict = {}
 
-#need to change ratings_value into an index that increments and use it to access ratings_list[index]
+
 for ratings_value in range(0, len(ratings_list)):
     if ratings_value % 2 == 0:
-        name = ratings_list[ratings_value]
+        name = str(ratings_list[ratings_value]).strip("\'[").strip("\']").replace('\', \'', " ")
+        # print(name)
     if ratings_value % 2 == 1:
         rating = ratings_list[ratings_value]
         ratings_lyst.append((name, rating))
@@ -65,21 +66,36 @@ def recommend(name_for_rec):
     for list2_values in rec_list[1]:
         compare_list2.append(int(list2_values))
 
-    for t_value in target_list:
-        t_index = t_index + 1
-        if t_value == 0:
-            if compare_list1[t_index - 1] >= 3 or compare_list2[t_index - 1] >= 3:
-                index_values.append(t_index - 1)
-            print(f"index: {t_index} number: {str(target_list[t_index - 1])}")
+    # for t_value in target_list:
+    #     t_index = t_index + 1
+    #     if t_value == 0:
+    #         if compare_list1[t_index - 1] >= 3 or compare_list2[t_index - 1] >= 3:
+    #             index_values.append(t_index - 1)
+    #             print(f"\t{str(book_list[t_index - 1])}")
+    recommended = []
+    for i in range(len(target_list)):
+        if target_list[i] == 0:
+            if compare_list1[i] >= 3 or compare_list2[i] >= 3:
+                recommended.append(tuple(book_list[i]))
+
+    r = sorted(recommended, key=lambda k: (k[0][k[0].rfind(" ") + 1:], k[0][:k[0].rfind(" ")], k[1]))
+
+    print(f"Recommendations for {name_for_rec} from {friends(name_for_rec)[0]} and {friends(name_for_rec)[1]}:")
+    for i_string_num in r:
+        author_string = i_string_num[0]
+        book_string = i_string_num[1]
+        print(f"\t{author_string}, {book_string}")
+
+    return r
+
+
+            # print(f"index: {t_index} number: {str(target_list[t_index - 1])}")
             # if compare_list1[target_list.index(t_value)] >= 3:
             #     print(str(compare_list1[target_list.index(t_value)]) + " " + str(target_list.index(t_value)))
             # if compare_list2[compare_list1.index(l1_value)] == 0:
             #     print(None)
-
             # print(l1_value, compare_list2[compare_list1.index(l1_value)])
 
-
-    return index_values
 
 
 
@@ -88,15 +104,26 @@ def recommend(name_for_rec):
 
 def main():
     """add stuff"""
+    name = input("Enter a reader's name: ").lower()
+    if name in ratings_dict:
+        recommend(name)
+    else:
+        print(f"No such reader as {name}")
+
+    # print(friends(name))
+    # print(recommend(name))
     # print(book_list)
     # print(ratings_list)
     # print(ratings_dict)
     # print(ratings_lyst)
-    print(dot_product("ben", "moose"))
-    print(friends("ben"))
-    print(ratings_dict.keys())
+    # print(dot_product("ben", "moose"))
+    # print(friends("ben"))
     # print(recommend('albus dumbledore'))
-    print(ratings_list)
+    # print(ratings_dict.keys())
+    # print(ratings_lyst)
+
+
+
 
 if __name__ == "__main__":
     main()
