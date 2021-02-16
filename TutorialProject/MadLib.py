8462015d19ab20e9ba62b24661b42bdd314e7173
Kaden(LenLap) <10836246@my.uvu.edu>
2021-02-15 23:32:48 -0700

    Added exercise 9a; deleted tutorial project

--- TutorialProject/MadLib.py
+++
@@ -1,7 +0,0 @@
-color = input("enter color: ")
-plural_noun = input("plural noun: ")
-celeb = input("celeb: ")
-
-print("Roses are " + color)
-print(plural_noun + " are blue")
-print("I love " + celeb)
