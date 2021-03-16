408dd7d4fc6f34b4a9eb658b25757bff1afbf402
Kaden(LenLap) <10836246@my.uvu.edu>
2021-03-16 00:48:16 -0600

    Worked on Book GUI

--- Python project/_Scripts/bookrecsgui.py
+++ Python project/_Scripts/bookrecsgui.py
@@ -15,23 +15,28 @@ class BookRecsGui(EasyFrame):
     def friend_action(self):
         value = self.prompterBox(title="Friends", promptString="Enter Reader Name.")
         try:
-            bookrecs.friends(value)
+            self.messageBox(title=f"Friends of {value}", message=f"{bookrecs.friends(value)[0]}\n{bookrecs.friends(value)[1]}")
         except KeyError:
             self.messageBox(title="Error", message="Incorrect name inserted.")
 
     def recommend_action(self):
         value = self.prompterBox(title="Recommend", promptString="Enter Reader Name.")
         try:
-            bookrecs.recommend(value)
             self.name_list.append(value)
+            self.messageBox(title=f"Recommendations for {value}", message=f"{bookrecs.recommend(value)}")
         except KeyError:
             self.messageBox(title="Error", message="Incorrect name inserted.")
 
     def report_action(self):
         try:
             self.name_list.sort()
+            report_string = ""
             for item in self.name_list:
-                self.report_list.append(bookrecs.recommend(item))
+                self.report_list.append(f"{bookrecs.recommend(item)}")
+
+            for i in range(len(self.report_list)):
+                report_string += f"{self.report_list[i][0]}, {self.report_list[i][1]}\n"
+            self.messageBox(title="Report", message=f"{self.report_list}", width=50, height=25)
         except KeyError:
             self.messageBox(title="Error", message="No names to report.")
 
