5db62ff4cf28b54f30b789ac90c1cdafd280bd30
Kaden(LenLap) <10836246@my.uvu.edu>
2021-03-16 22:00:27 -0600

    Finished GUI

--- Python project/_Scripts/bookrecsgui.py
+++ Python project/_Scripts/bookrecsgui.py
@@ -6,7 +6,6 @@ class BookRecsGui(EasyFrame):
     def __init__(self):
         EasyFrame.__init__(self, title="Book Recommendations", background="#B0E0E6")
         self.name_list = []
-        self.report_list = []
 
         self.addButton(text="Friends", row=0, column=0, command=self.friend_action)
         self.addButton(text="Recommend", row=0, column=1, command=self.recommend_action)
@@ -17,28 +16,37 @@ class BookRecsGui(EasyFrame):
         try:
             self.messageBox(title=f"Friends of {value}", message=f"{bookrecs.friends(value)[0]}\n{bookrecs.friends(value)[1]}")
         except KeyError:
-            self.messageBox(title="Error", message="Incorrect name inserted.")
+            self.messageBox(title="Error", message="No such reader.")
 
     def recommend_action(self):
         value = self.prompterBox(title="Recommend", promptString="Enter Reader Name.")
         try:
             self.name_list.append(value)
-            self.messageBox(title=f"Recommendations for {value}", message=f"{bookrecs.recommend(value)}")
+            report_string = ""
+            for books in bookrecs.recommend(value):
+                report_string += f"{books[0]}, {books[1]}\n"
+
+            self.messageBox(title=f"Recommendations for {value}", message=f"{report_string}", width=60, height=25)
         except KeyError:
-            self.messageBox(title="Error", message="Incorrect name inserted.")
+            self.messageBox(title="Error", message="No such reader.")
 
     def report_action(self):
         try:
             self.name_list.sort()
             report_string = ""
             for item in self.name_list:
-                self.report_list.append(f"{bookrecs.recommend(item)}")
+                if item in bookrecs.ratings_dict.keys():
+                    report_string += f"Recommendations for {item} from {bookrecs.friends(item)[0]} and {bookrecs.friends(item)[1]}:\n"
+                    for books in bookrecs.recommend(item):
+                        report_string += f"\t{books[0]}, {books[1]}\n"
+                    report_string += "\n"
+                
+            if len(self.name_list) == 0:
+                report_string = "No readers to report."
 
-            for i in range(len(self.report_list)):
-                report_string += f"{self.report_list[i][0]}, {self.report_list[i][1]}\n"
-            self.messageBox(title="Report", message=f"{self.report_list}", width=50, height=25)
+            self.messageBox(title="Report", message=f"{report_string}", width=60, height=25)
         except KeyError:
-            self.messageBox(title="Error", message="No names to report.")
+            self.messageBox(title="Error", message="No such reader.")
 
 
 def main():
